1ea63fa66d1e61b9c1d614d71a5602d5aacbadc1
Viktoria <koroleva_vs02@mail.ru>
2022-02-22 19:17:51 +0300

    добавили обновленный SQL скрипт для создания базы данных протестировали dao

--- sql/init.sql
+++ sql/init.sql
@@ -2,28 +2,28 @@ CREATE DATABASE post;
 
 CREATE TABLE PostOffice(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(60),
-    address VARCHAR(80)
+    name VARCHAR(60) NOT NULL ,
+    address VARCHAR(80) NOT NULL
 );
-DROP TABLE Employee;
+
 CREATE TABLE Employee(
     id SERIAL PRIMARY KEY,
-    fullName VARCHAR(70),
-    phone VARCHAR(20),
+    fullName VARCHAR(70) NOT NULL ,
+    phone VARCHAR(20) NOT NULL ,
     id_post INT NOT NULL ,
     FOREIGN KEY (id_post) REFERENCES PostOffice(id)
 );
 
 CREATE TABLE Customer (
     id SERIAL PRIMARY KEY,
-    fullName VARCHAR(60),
+    fullName VARCHAR(60) NOT NULL ,
     phone VARCHAR(70) UNIQUE,
-    address VARCHAR(70)
+    address VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE Package (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(60),
+    name VARCHAR(60) NOT NULL ,
     trackNumber VARCHAR(60) UNIQUE,
     id_recipient INTEGER REFERENCES Customer(id),
     id_sender INTEGER REFERENCES Customer(id)
@@ -32,5 +32,5 @@ CREATE TABLE Package (
 CREATE TABLE Location(
     id SERIAL PRIMARY KEY,
     trackNumber VARCHAR(60) UNIQUE REFERENCES Package(trackNumber),
-    location VARCHAR(60)
+    location VARCHAR(60) NOT NULL
 );
